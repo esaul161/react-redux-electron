257f99d40faab25ef2d1cb5485bb75e94597950f
Esaul Esparza Flores <esaul.esparza@globant.com>
2019-11-13 16:04:34 -0600

    react redux electron app

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import testReducer from './test';
+
+const rootReducer = combineReducers({
+  testReducer: testReducer,
+});
+
+export default rootReducer;
